a101882b7702a30895bd3749610bc7177f3d594f
Maria <mari.shumilova.96@mail.ru>
2022-07-08 16:24:12 +0300

    forms

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -12,19 +12,19 @@ const modals = () => {
                 }
 
                 modal.style.display = 'block';
-                document.body.overflow = 'hidden';
+                document.body.style.overflow = 'hidden';
             });
         });
 
         close.addEventListener('click', () => {
             modal.style.display = 'none';
-            document.body.overflow = '';
+            document.body.style.overflow = '';
         });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
                 modal.style.display = 'none';
-                document.body.overflow = '';
+                document.body.style.overflow = '';
             }
         });
     }
@@ -32,7 +32,7 @@ const modals = () => {
     function showModalByTime(selector, time) {
         setTimeout(function () {
             document.querySelector(selector).style.display = 'block';
-            document.body.overflow = 'hidden';
+            document.body.style.overflow = 'hidden';
         }, time);
     }
 
