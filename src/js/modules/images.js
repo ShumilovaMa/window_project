4618d5454e6a2859aa5855c278e7029bc65ab931
Maria <mari.shumilova.96@mail.ru>
2022-07-10 13:57:39 +0300

    revision

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -2,16 +2,15 @@ const images = () => {
     const imgPopup = document.createElement('div'),
           workSection = document.querySelector('.works'),
           bigImg = document.createElement('img');
-
+    
     imgPopup.classList.add('popup');
 
     workSection.appendChild(imgPopup);
 
     imgPopup.style.justifyContent = 'center';
     imgPopup.style.alignItems = 'center';
-    imgPopup.style.display = 'none';
+    imgPopup.style.display = 'none';   
     
-
     imgPopup.appendChild(bigImg);
 
     workSection.addEventListener('click', (e) => {
@@ -20,14 +19,14 @@ const images = () => {
         let target = e.target;
 
         if (target && target.classList.contains('preview')) {
-            imgPopup.style.display = 'flex';
+            imgPopup.style.display = 'flex'; 
             const path = target.parentNode.getAttribute('href');
             bigImg.setAttribute('src', path);
             document.body.style.overflow = 'hidden';
         }
 
         if (target && target.matches('div.popup')) {
-            imgPopup.style.display = 'none';
+            imgPopup.style.display = 'none'; 
             document.body.style.overflow = '';
         }
     });
